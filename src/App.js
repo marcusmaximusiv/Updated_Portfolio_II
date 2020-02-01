d6a60b9ddcb8debbd6d17e3543cad80d0d53af52
Sterrenberg, Mark <mark.allen.sterrenberg@gmail.com>
2020-01-31 23:59:18 -0600

    getting the routing and everything to work for the application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Bootstrap from 'bootstrap';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import Home from "./components/pages/Home";
