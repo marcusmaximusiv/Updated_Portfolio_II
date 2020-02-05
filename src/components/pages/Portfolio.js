4d6c2c9925b210917f2b53e24fda1e152853a45d
Sterrenberg, Mark <mark.allen.sterrenberg@gmail.com>
2020-02-04 23:51:25 -0600

    add links in portfolio to actual projects and provided images

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -30,47 +30,47 @@ function Portfolio() {
                     </div>
                     <div className="portfolioContainer wow fadeInUp" data-wow-delays="04s">
                         <div className=" Portfolio-box html webdesign css">
-                            <a href="../../img/Portfolio-pic1.jpg"><img src="../../img/Portfolio-pic1.jpg" alt="" /></a>
+                            <a href="https://marcusmaximusiv.github.io/Responsive-Portfolio"><img src="../../img/Portfolio-pic1.jpg" alt="" /></a>
                             <h3>Building a Wireframe/That Portfolio Though</h3>
                             <p>HTML, CSS, Web Design</p>
                         </div>
                         <div className="Portfolio-box webdesign html css">
-                            <a href="../img/Portfolio-pic2.jpg"><img src="img/Portfolio-pic2.jpg" alt="" /></a>
+                            <a href="https://marcusmaximusiv.github.io/Bootstrap-Portfolio"><img src="img/Portfolio-pic2.jpg" alt="" /></a>
                             <h3>Responsive Assignment</h3>
                             <p>Web Design, HTML, CSS</p>
                         </div>
                         <div className=" Portfolio-box html css javascript">
-                            <a href="img/Portfolio-pic3.jpg"><img src="img/Portfolio-pic3.jpg" alt="" /></a>
+                            <a href="https://marcusmaximusiv.github.io/03_JavaScript_Assignment/"><img src="img/Portfolio-pic3.jpg" alt="" /></a>
                             <h3>Javascript Assignment</h3>
                             <p>HTML, CSS, Javascript</p>
                         </div>
                         <div className=" Portfolio-box html css jquery">
-                            <a href="img/Portfolio-pic4.jpg"><img src="img/Portfolio-pic4.jpg" alt="" /></a>
+                            <a href="https://marcusmaximusiv.github.io/10-Liri-bot/"><img src="img/Portfolio-pic4.jpg" alt="" /></a>
                             <h3>JQuery Assignment</h3>
                             <p>HTML, CSS, Jquery</p>
                         </div>
                         <div className=" Portfolio-box html css javascript">
-                            <a href="img/Portfolio-pic5.jpg"><img src="img/Portfolio-pic5.jpg" alt="" /></a>
+                            <a href="https://marcusmaximusiv.github.io/11-advanced-javascript/"><img src="img/Portfolio-pic5.jpg" alt="" /></a>
                             <h3>Javascript Assignment 2</h3>
                             <p>HTML, CSS, Javascript</p>
                         </div>
                         <div className=" Portfolio-box ajax css html">
-                            <a href="img/Portfolio-pic6.jpg"><img src="img/Portfolio-pic6.jpg" alt="" /></a>
+                            <a href="https://marcusmaximusiv.github.io/06-GIPHY-API"><img src="img/Portfolio-pic6.jpg" alt="" /></a>
                             <h3>GifTastic</h3>
                             <p>HTML, CSS, Jquery, AJAX</p>
                         </div>
                         <div className=" Portfolio-box jquery css html">
-                            <a href="img/Portfolio-pic6.jpg"><img src="img/Portfolio-pic6.jpg" alt="" /></a>
+                            <a href="https://marcusmaximusiv.github.io/07-Train-Scheduler/"><img src="img/Portfolio-pic6.jpg" alt="" /></a>
                             <h3>Train Scheduler</h3>
                             <p>HTML, CSS, Jquery</p>
                         </div>
                         <div className=" Portfolio-box jquery css html ajax projects">
-                            <a href="img/Portfolio-pic6.jpg"><img src="img/Portfolio-pic6.jpg" alt="" /></a>
+                            <a href="https://marcusmaximusiv.github.io/Project_1/"><img src="img/Portfolio-pic6.jpg" alt="" /></a>
                             <h3>Project 1</h3>
                             <p>HTML, CSS, Jquery, AJAX</p>
                         </div>
                         <div className=" Portfolio-box jquery css html javascript">
-                            <a href="img/Portfolio-pic6.jpg"><img src="img/Portfolio-pic6.jpg" alt="" /></a>
+                            <a href="https://marcusmaximusiv.github.io/Updated_Portfolio/"><img src="img/Portfolio-pic6.jpg" alt="" /></a>
                             <h3>Updated Portfolio</h3>
                             <p>HTML, CSS, Jquery, javascript</p>
                         </div>
